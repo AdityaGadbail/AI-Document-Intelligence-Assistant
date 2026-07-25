25b4c30b78966b82c874d7a1c71b3ba33817f9f5
Aditya Gadbail <163659268+AdityaGadbail@users.noreply.github.com>
2026-07-25 20:57:40 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,10 +1,18 @@
+from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from database.base import Base
+
+
+Path("storage").mkdir(parents=True, exist_ok=True)
+Path("storage/uploads").mkdir(parents=True, exist_ok=True)
+Path("storage/vector_store").mkdir(parents=True, exist_ok=True)
 DATABASE_URL = "sqlite:///storage/app.db"
 
 engine = create_engine(
     DATABASE_URL,
-    echo = False
+    echo = False,
+    connect_args={"check_same_thread": False},
 )
 
 SessionLocal = sessionmaker(
@@ -13,9 +21,14 @@ SessionLocal = sessionmaker(
     autocommit = False
 )
 
+def init_db():
+    import models
+    Base.metadata.create_all(bind=engine)
+    
+init_db()
+
 def get_db():
     db = SessionLocal()
-
     try:
         yield db
     finally:
